6b2ace911e2c9236953b0909057c2e99ac05f0f3
AnaCarolina2M <anacarolinambsb@gmail.com>
2026-04-16 00:09:51 -0300

    feat: class MLFlowLog

---
+++ mlflow_log/mlflow_log.py
@@ -0,0 +1,26 @@
+from matplotlib.pyplot import grid
+from sklearn.model_selection import GridSearchCV
+from strategies.mlflow_strategy import IMLFlowLog
+from mlflow import mlflow
+import mlflow.sklearn
+from sklearn.metrics import accuracy_score
+
+class MlFlowLog(IMLFlowLog):
+    def __init__(self,model, experiment_name, param_grid: dict):
+        self.model = model
+        self.experiment_name = experiment_name
+        self.param_grid = param_grid
+        self.grid = None
+
+    def experiment(self, X_train, y_train, X_test, y_test):
+        print("Logging experiment details to MLflow...")
+        mlflow.set_experiment(experiment_name=self.experiment_name)
+        with mlflow.start_run():
+            self.grid = GridSearchCV(self.model, self.param_grid, cv=5)
+            self.grid.fit(X_train, y_train)
+            y_pred = self.grid.predict(X_test)
+            mlflow.log_params(self.grid.best_params_)
+            mlflow.log_metric("best_cv_score", self.grid.best_score_)
+            acc = accuracy_score(y_test, y_pred)
+            mlflow.log_metric("test_accuracy", acc)
+            mlflow.sklearn.log_model(self.grid.best_estimator_, "model")
